5359e08df248eba7bd677fbd8f0106c605c7fd1c
viza <vzaytsev@my.centennialcollege.ca>
2024-09-19 20:48:15 -0400

    add courseInfo endpoint

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/bye', (req, res) => {
    res.send('Bye World!')
 })
 
+app.get('/courseInfo', (req, res) => {
+   res.send('COMP229 Web App Development')
+})
+
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
 module.exports = app;
